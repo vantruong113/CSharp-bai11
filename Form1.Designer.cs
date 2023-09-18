371d9ab90442b27846b53785e1b5dd9e5aecc1bb
ADMIN <ADMIN@LAB1-MAY35>
2023-09-18 16:41:22 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CSharp_bai11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttenquydoi = new System.Windows.Forms.TextBox();
+            this.txtketqua = new System.Windows.Forms.TextBox();
+            this.btnVNDtoUSD = new System.Windows.Forms.Button();
+            this.btnVNDtoEUR = new System.Windows.Forms.Button();
+            this.btnUSDtoVND = new System.Windows.Forms.Button();
+            this.btnEURtoVND = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(284, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐỔI TIỀN ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tiền quy đổi ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "kết quả ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txttenquydoi
+            // 
+            this.txttenquydoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttenquydoi.Location = new System.Drawing.Point(303, 144);
+            this.txttenquydoi.Name = "txttenquydoi";
+            this.txttenquydoi.Size = new System.Drawing.Size(262, 38);
+            this.txttenquydoi.TabIndex = 3;
+            this.txttenquydoi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtketqua.Location = new System.Drawing.Point(291, 324);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(262, 38);
+            this.txtketqua.TabIndex = 4;
+            // 
+            // btnVNDtoUSD
+            // 
+            this.btnVNDtoUSD.Location = new System.Drawing.Point(61, 232);
+            this.btnVNDtoUSD.Name = "btnVNDtoUSD";
+            this.btnVNDtoUSD.Size = new System.Drawing.Size(94, 23);
+            this.btnVNDtoUSD.TabIndex = 5;
+            this.btnVNDtoUSD.Text = "VNDtoUSD";
+            this.btnVNDtoUSD.UseVisualStyleBackColor = true;
+            // 
+            // btnVNDtoEUR
+            // 
+            this.btnVNDtoEUR.Location = new System.Drawing.Point(194, 232);
+            this.btnVNDtoEUR.Name = "btnVNDtoEUR";
+            this.btnVNDtoEUR.Size = new System.Drawing.Size(95, 23);
+            this.btnVNDtoEUR.TabIndex = 6;
+            this.btnVNDtoEUR.Text = "VNDtoEUR";
+            this.btnVNDtoEUR.UseVisualStyleBackColor = true;
+            // 
+            // btnUSDtoVND
+            // 
+            this.btnUSDtoVND.Location = new System.Drawing.Point(354, 232);
+            this.btnUSDtoVND.Name = "btnUSDtoVND";
+            this.btnUSDtoVND.Size = new System.Drawing.Size(89, 23);
+            this.btnUSDtoVND.TabIndex = 7;
+            this.btnUSDtoVND.Text = "USDtoVND";
+            this.btnUSDtoVND.UseVisualStyleBackColor = true;
+            // 
+            // btnEURtoVND
+            // 
+            this.btnEURtoVND.Location = new System.Drawing.Point(490, 232);
+            this.btnEURtoVND.Name = "btnEURtoVND";
+            this.btnEURtoVND.Size = new System.Drawing.Size(103, 23);
+            this.btnEURtoVND.TabIndex = 8;
+            this.btnEURtoVND.Text = "EURtoVND";
+            this.btnEURtoVND.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEURtoVND);
+            this.Controls.Add(this.btnUSDtoVND);
+            this.Controls.Add(this.btnVNDtoEUR);
+            this.Controls.Add(this.btnVNDtoUSD);
+            this.Controls.Add(this.txtketqua);
+            this.Controls.Add(this.txttenquydoi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttenquydoi;
+        private System.Windows.Forms.TextBox txtketqua;
+        private System.Windows.Forms.Button btnVNDtoUSD;
+        private System.Windows.Forms.Button btnVNDtoEUR;
+        private System.Windows.Forms.Button btnUSDtoVND;
+        private System.Windows.Forms.Button btnEURtoVND;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_bai11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Length > 0 && !Char.IsDigit(ctr.Text[ctr.Text.Length - 1]))
+                this.errorProvider1.SetError(ctr, "this is not avalid number") ;
+            else
+                this.errorProvider1.Clear() ;
+     
+        }
+    }
+}
